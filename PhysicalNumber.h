e2c9a6c1623eed8a35f4e0ed9d201b3a86950ec7
maxkrakauer <maxkrakauer91@gmail.com>
2019-04-16 13:34:23 -0400

    Add files via upload

--- PhysicalNumber.h
+++ PhysicalNumber.h
@@ -98,13 +98,11 @@ string unitToStr(Unit unit);
 
      PhysicalNumber operator++(PhysicalNumber &a,int);
 
-     PhysicalNumber operator++(PhysicalNumber &a,int);
-
      PhysicalNumber operator--(PhysicalNumber &a, int);
 
      PhysicalNumber operator++(ariel::PhysicalNumber &a);
 
-     PhysicalNumber operator--(PhysicalNumber &a);
+     PhysicalNumber operator--(ariel::PhysicalNumber &a);
 
      PhysicalNumber strToNum(string str);
 
